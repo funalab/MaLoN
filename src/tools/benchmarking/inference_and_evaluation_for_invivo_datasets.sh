350dc152456a25e0d02bfa0877ecaa6ad72f25b6
Yusuke Hiki <hiki@fun.bio.keio.ac.jp>
2024-07-22 15:19:41 +0900

    Datasets for benchmarking were added.

--- src/tools/benchmarking/inference_and_evaluation_for_invivo_datasets.sh
+++ src/tools/benchmarking/inference_and_evaluation_for_invivo_datasets.sh
@@ -10,64 +10,6 @@ touch ${LOG_FILE}
 DYN_REG_PLOT=../../tools/plot_dynamic_regulation_map.py
 # ====================================================
 
-# DDGni_E datase ===
-echo "=== DDGni E dataset ==="
-# inference
-INPUT=../datasets/ddgni_datasets/DDGni_E.txt
-DSNAME=DDGni_E
-OUTPUT_DIR=${RESULT_DIR}/${DSNAME}/
-python main_${MODEL}.py ${INPUT} --config ${CFG} --name ${DSNAME}
-cp ${CFG} ${OUTPUT_DIR}
-
-# evaluation
-TRUE_EDGELIST_FILE=../datasets/ddgni_datasets/DDGni_E_network.txt
-PRED_EDGELIST_FILE=${OUTPUT_DIR}/${DSNAME}_edgelist.tsv
-python evaluation.py ${PRED_EDGELIST_FILE} ${TRUE_EDGELIST_FILE} ${DSNAME} ${OUTPUT_DIR} >> ${LOG_FILE}
-
-# visualization
-DYN_REG_MAP_FILE=${OUTPUT_DIR}/${DSNAME}_dynamic_regulation_map.tsv
-OUTPUT_DYN_REG_MAP_DIR=${OUTPUT_DIR}/dynamic_regulation_map
-python ${DYN_REG_PLOT} ${DYN_REG_MAP_FILE} --type source --out ${OUTPUT_DYN_REG_MAP_DIR} --name ${DSNAME}
-python ${DYN_REG_PLOT} ${DYN_REG_MAP_FILE} --type target --out ${OUTPUT_DYN_REG_MAP_DIR} --name ${DSNAME}
-# ==================
-
-# DDGni_Yeast dataset ===
-echo "=== DDGni Yeast dataset ==="
-# inference
-INPUT=../datasets/ddgni_datasets/DDGni_Yeast.txt
-DSNAME=DDGni_Yeast
-OUTPUT_DIR=${RESULT_DIR}/${DSNAME}/
-python main_${MODEL}.py ${INPUT} --config ${CFG} --name ${DSNAME}
-cp ${CFG} ${OUTPUT_DIR}
-
-# evaluation
-TRUE_EDGELIST_FILE=../datasets/ddgni_datasets/DDGni_Yeast_network.txt
-PRED_EDGELIST_FILE=${OUTPUT_DIR}/${DSNAME}_edgelist.tsv
-python evaluation.py ${PRED_EDGELIST_FILE} ${TRUE_EDGELIST_FILE} ${DSNAME} ${OUTPUT_DIR} >> ${LOG_FILE}
-# =======================
-
-# E.coli SOS pathway dataset ===
-echo "=== Ecoli SOS pathway dataset ==="
-# inference
-INPUT=../datasets/ecoli_sospathway_datasets/ecoli_sospathway_concatenated.tsv
-DSNAME=Ecoli_SOSPathway
-OUTPUT_DIR=${RESULT_DIR}/${DSNAME}/
-python main_${MODEL}.py ${INPUT} --config ${CFG} --name ${DSNAME}
-cp ${CFG} ${OUTPUT_DIR}
-
-# evaluation
-TRUE_EDGELIST_FILE=../datasets/ecoli_sospathway_datasets/Network_Ecoli_SOSPathway.txt
-PRED_EDGELIST_FILE=${OUTPUT_DIR}/${DSNAME}_edgelist.tsv
-python evaluation.py ${PRED_EDGELIST_FILE} ${TRUE_EDGELIST_FILE} ${DSNAME} ${OUTPUT_DIR} >> ${LOG_FILE}
-
-# visualization
-DYN_REG_MAP_FILE=${OUTPUT_DIR}/${DSNAME}_dynamic_regulation_map.tsv
-OUTPUT_DYN_REG_MAP_DIR=${OUTPUT_DIR}/dynamic_regulation_map
-python ${DYN_REG_PLOT} ${DYN_REG_MAP_FILE} --type source --out ${OUTPUT_DYN_REG_MAP_DIR} --name ${DSNAME}
-python ${DYN_REG_PLOT} ${DYN_REG_MAP_FILE} --type target --out ${OUTPUT_DYN_REG_MAP_DIR} --name ${DSNAME}
-# ===============================
-
-
 # IRMA switch-on dataset ===
 echo "=== IRMA switch-on dataset ==="
 # inference
@@ -111,39 +53,17 @@ python ${DYN_REG_PLOT} ${DYN_REG_MAP_FILE} --type source --out ${OUTPUT_DYN_REG_
 python ${DYN_REG_PLOT} ${DYN_REG_MAP_FILE} --type target --out ${OUTPUT_DYN_REG_MAP_DIR} --name ${DSNAME}
 # ==========================
 
-
-# Spellman cdc15 dataset ===
-echo "=== Spellman cdc15 dataset ==="
+# E.coli SOS pathway dataset ===
+echo "=== Ecoli SOS pathway dataset ==="
 # inference
-INPUT=../datasets/spellman_datasets/Spellman_cdc15.txt
-DSNAME=Spellman_cdc15
-OUTPUT_DIR=${RESULT_DIR}/${DSNAME}/
-python main_${MODEL}.py ${INPUT} --config ${CFG} --name ${DSNAME}
-cp ${CFG} ${OUPUT_DIR}
-
-# evaluation
-TRUE_EDGELIST_FILE=../datasets/spellman_datasets/Spellman_Yeast9_network.tsv
-PRED_EDGELIST_FILE=${OUTPUT_DIR}/${DSNAME}_edgelist.tsv
-python evaluation.py ${PRED_EDGELIST_FILE} ${TRUE_EDGELIST_FILE} ${DSNAME} ${OUTPUT_DIR} >> ${LOG_FILE}
-
-# visualization
-DYN_REG_MAP_FILE=${OUTPUT_DIR}/${DSNAME}_dynamic_regulation_map.tsv
-OUTPUT_DYN_REG_MAP_DIR=${OUTPUT_DIR}/dynamic_regulation_map
-python ${DYN_REG_PLOT} ${DYN_REG_MAP_FILE} --type source --out ${OUTPUT_DYN_REG_MAP_DIR} --name ${DSNAME}
-python ${DYN_REG_PLOT} ${DYN_REG_MAP_FILE} --type target --out ${OUTPUT_DYN_REG_MAP_DIR} --name ${DSNAME}
-# ==========================
-
-# Spellman cdc28 dataset ===
-echo "=== Spellman cdc28 dataset ==="
-# inference
-INPUT=../datasets/spellman_datasets/Spellman_cdc28.txt
-DSNAME=Spellman_cdc28
+INPUT=../datasets/ecoli_sospathway_datasets/ecoli_sospathway_concatenated.tsv
+DSNAME=Ecoli_SOSPathway
 OUTPUT_DIR=${RESULT_DIR}/${DSNAME}/
 python main_${MODEL}.py ${INPUT} --config ${CFG} --name ${DSNAME}
 cp ${CFG} ${OUTPUT_DIR}
 
 # evaluation
-TRUE_EDGELIST_FILE=../datasets/spellman_datasets/Spellman_Yeast9_network.tsv
+TRUE_EDGELIST_FILE=../datasets/ecoli_sospathway_datasets/Network_Ecoli_SOSPathway.txt
 PRED_EDGELIST_FILE=${OUTPUT_DIR}/${DSNAME}_edgelist.tsv
 python evaluation.py ${PRED_EDGELIST_FILE} ${TRUE_EDGELIST_FILE} ${DSNAME} ${OUTPUT_DIR} >> ${LOG_FILE}
 
@@ -152,7 +72,7 @@ DYN_REG_MAP_FILE=${OUTPUT_DIR}/${DSNAME}_dynamic_regulation_map.tsv
 OUTPUT_DYN_REG_MAP_DIR=${OUTPUT_DIR}/dynamic_regulation_map
 python ${DYN_REG_PLOT} ${DYN_REG_MAP_FILE} --type source --out ${OUTPUT_DYN_REG_MAP_DIR} --name ${DSNAME}
 python ${DYN_REG_PLOT} ${DYN_REG_MAP_FILE} --type target --out ${OUTPUT_DYN_REG_MAP_DIR} --name ${DSNAME}
-# ==========================
+# ===============================
 
 mv ${LOG_FILE} ${RESULT_DIR}
 
